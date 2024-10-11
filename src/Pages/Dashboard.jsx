d7a5ca6fd176e1d6caf3cd2dfae10e02d1ac8f83
Banti Singh <banti.singh@mysetu.com>
2024-10-11 14:36:23 +0530

    new side bar implement

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { sessiondata } from '../Context/Context';
 import {SideBar, NavBar} from '../Dashboard/SideBar';
-import AdminSummary from '../Dashboard/AdminSummary';
+//import AdminSummary from '../Dashboard/AdminSummary';
 import { Outlet } from 'react-router-dom';
+import { MultiLevelSidebar } from '../Dashboard/demotest';
 
 const DashboardPage = () => {
     const { user } = sessiondata();
   return (
     <div className='flex'>
-        <SideBar/>  
+        {/* <SideBar/>   */}
+
+        <MultiLevelSidebar />
         <div className='flex-1 ml-64 bg-gray-100 h-screen'>
             <NavBar/>
             <Outlet/>
